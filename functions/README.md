d16ec78fb2e1f21e56f88250c90021da8f931b65
Dustin Haggard <dustin@dustinhaggard.com>
2014-08-31 14:16:32 -0400

    Combine function parameters of the same type

--- functions/README.md
+++ functions/README.md
@@ -3,3 +3,4 @@
 ## What I learned
 
 - How to write functions (again)
+- How to combine consecutive parameters that share the same type
--- functions/functions.go
+++ functions/functions.go
@@ -10,7 +10,18 @@ func sub(x int, y int) int {
     return x - y
 }
 
+func addCondense(x, y int) int {
+    return x + y
+}
+
+func subCondense(x, y int) int {
+    return x - y
+}
+
 func main() {
     fmt.Println(add(2, 2))
     fmt.Println(sub(100, 10))
+    
+    fmt.Println(addCondense(2, 2))
+    fmt.Println(subCondense(100, 10))
 }
