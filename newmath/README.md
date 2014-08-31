4c5425a741431433f33c030dfbcd21a609a9569e
Dustin Haggard <dustin@dustinhaggard.com>
2014-08-31 14:01:21 -0400

    Added unit testing

--- newmath/README.md
+++ newmath/README.md
@@ -3,3 +3,4 @@
 ## What I learned
 
 - How to write a function that approximates the square root of a given number
+- How to test a function with the "testing" package
---
+++ newmath/sqrt_test.go
@@ -0,0 +1,12 @@
+// http://golang.org/doc/code.html
+
+package newmath
+
+import "testing"
+
+func TestSqrt(t *testing.T) {
+    const in, out = 4, 2
+    if x := Sqrt(in); x != out {
+        t.Errorf("Sqrt(%v) = %v, want %v", in, x, out)
+    }
+}
