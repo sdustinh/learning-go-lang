eddb50ac3c1f97bc669ac7e30526d34ecf11c3f4
Dustin Haggard <dustin@dustinhaggard.com>
2014-08-31 14:22:18 -0400

    Split into another file

---
+++ functions/combined_type.go
@@ -0,0 +1,9 @@
+package main
+
+func addCondense(x, y int) int {
+    return x + y
+}
+
+func subCondense(x, y int) int {
+    return x - y
+}
--- functions/functions.go
+++ functions/functions.go
@@ -10,14 +10,6 @@ func sub(x int, y int) int {
     return x - y
 }
 
-func addCondense(x, y int) int {
-    return x + y
-}
-
-func subCondense(x, y int) int {
-    return x - y
-}
-
 func main() {
     fmt.Println(add(2, 2))
     fmt.Println(sub(100, 10))
