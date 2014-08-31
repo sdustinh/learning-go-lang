8971a50cab85ede2d8bef270fdc72ab532943ce3
Dustin Haggard <dustin@dustinhaggard.com>
2014-08-31 13:46:56 -0400

    Copied hello world package to import newmath package

---
+++ hellonewmath/hello.go
@@ -0,0 +1,12 @@
+// http://golang.org/doc/install
+
+package main
+
+import (
+    "fmt"
+    "github.com/sdustinh/newmath"
+)
+
+func main() {
+    fmt.Printf("Hello, world. Sqrt(2) = %v\n", newmath.Sqrt(2))
+}
